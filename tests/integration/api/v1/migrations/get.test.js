690bec37556ff59161d536822c3e23333643948e
Erickson Luiz Delfino <101117503+EricksonLuiz@users.noreply.github.com>
2026-01-07 14:06:50 -0300

    Adicionar log ppara forçar deploy de homologacao

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -14,4 +14,6 @@ test("GET to /api/v1/migrations should return 200", async () => {
 
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
+
+  console.log("console para deploy de homologação");
 });
